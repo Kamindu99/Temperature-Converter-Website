a9aa9c93b324419860ab50fa60c7265df7130d90
kamindu.g <kamindu.g@informaticsint.com>
2023-07-12 15:59:23 +0530

    cerate html page

---
+++ tempwebJS.js
@@ -0,0 +1,21 @@
+function temperatureConverter(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("outputCelcius").innerHTML = (valNum - 32) / 1.8;
+}
+
+function temperatureConverterCtoF(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("outputFahrenheit").innerHTML = (valNum * 1.8) + 32;
+}
+
+function toggleConversion(conversionType) {
+  if (conversionType === 'celsius') {
+    document.getElementById('celsiusDiv').style.display = 'block';
+    document.getElementById('fahrenheitDiv').style.display = 'none';
+    document.getElementById('inputFahrenheit').innerHTML='';
+  } else if (conversionType === 'fahrenheit') {
+    document.getElementById('celsiusDiv').style.display = 'none';
+    document.getElementById('fahrenheitDiv').style.display = 'block';
+    document.getElementById('inputCelsius').innerHTML='';
+  }
+}
