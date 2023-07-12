d273e613c6b53a2501dbb30e8da31fa22f118475
kamindu.g <kamindu.g@informaticsint.com>
2023-07-12 18:36:25 +0530

    currency convert

--- tempwebJS.js
+++ tempwebJS.js
@@ -1,11 +1,12 @@
+// Temperature Conversion
 function temperatureConverter(valNum) {
   valNum = parseFloat(valNum);
-  document.getElementById("outputCelcius").innerHTML = (valNum - 32) / 1.8;
+  document.getElementById("outputCelcius").innerHTML = `${((valNum - 32) / 1.8).toFixed(2)} C` ;
 }
 
 function temperatureConverterCtoF(valNum) {
   valNum = parseFloat(valNum);
-  document.getElementById("outputFahrenheit").innerHTML = (valNum * 1.8) + 32;
+  document.getElementById("outputFahrenheit").innerHTML = `${((valNum * 1.8) + 32).toFixed(2)} F` ;
 }
 
 function toggleConversion(conversionType) {
@@ -21,3 +22,31 @@ function toggleConversion(conversionType) {
     document.getElementById("outputFahrenheit").innerHTML = '';
   }
 }
+
+// Currency Conversion
+
+function currencyConverterRtoD(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("outputdoller").innerHTML = `$ ${((valNum) / 320).toFixed(2)}` ;
+}
+
+function currencyConverterDtoR(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("outputrupees").innerHTML = `Rs. ${((valNum) * 320).toFixed(2)}` ;
+}
+
+function toggleConversionCurr(conversionType) {
+  if (conversionType === 'doller') {
+    document.getElementById('dollerDiv').style.display = 'block';
+    document.getElementById('rupeesDiv').style.display = 'none';
+    document.getElementById('inputrupees').value = '';
+    document.getElementById("outputdoller").innerHTML = '';
+  } else if (conversionType === 'rupees') {
+    document.getElementById('dollerDiv').style.display = 'none';
+    document.getElementById('rupeesDiv').style.display = 'block';
+    document.getElementById('inputdoller').value = '';
+    document.getElementById("outputrupees").innerHTML = '';
+  }
+}
+
+
